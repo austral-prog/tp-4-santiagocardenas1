9fe0ba4436e033f849af401e436ce48a90c46a4f
santiagocardenas1 <scardenas@mail.austral.edu.ar>
2025-04-10 20:47:47 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+    año = int(input(""))
+    bi = ((año%4) == 0 and (año%100) != 0) or (año%400) == 0
+    if bi == True:
+        print(f"El año {año} es bisiesto")
+    else:
+        print(f"El año {año} no es bisiesto")
